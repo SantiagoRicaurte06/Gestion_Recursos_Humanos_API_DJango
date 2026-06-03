fd492bdf51f79e5fc8fa1c34064cf58408b24461
SantiagoRicaurte06 <santiagoricuarte@gmail.com>
2026-06-03 16:17:16 -0500

    Se arregla ruta del swagger

--- backend/urls.py
+++ backend/urls.py
@@ -18,7 +18,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
